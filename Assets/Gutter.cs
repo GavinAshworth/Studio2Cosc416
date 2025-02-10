3935a3e0381ddab3c57678b3af483257c4757389
GavinAshworth <gavinashworth315@gmail.com>
2025-02-09 18:38:28 -0800

    Added fall detection and small bug fixes
    Pins can detect their falls, also only ball interacts with the gutter, also ball cant overlap into gutter anymore while in the players hands

--- Assets/Gutter.cs
+++ Assets/Gutter.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class Gutter : MonoBehaviour
 {
    private void OnTriggerEnter(Collider triggeredBody){
+    // Check if the object has the "Ball" tag(added this check to stop pins from triggering this)
+    if (!triggeredBody.CompareTag("Ball")) return;
     Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+    //This check stops the code from trying to change the velocity of the ball when it is still in the players hands, happens sometimes when the ball gets too close to the gutter while in the hands of the player
+    if(ballRigidBody.isKinematic){
+        return;
+    }
     float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
 
     ballRigidBody.linearVelocity = Vector3.zero;
