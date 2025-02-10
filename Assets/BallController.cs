b94d74cfa286774651f7f8fdc7857c04eb9e2ab2
GavinAshworth <gavinashworth315@gmail.com>
2025-02-09 17:06:30 -0800

    Ball Moves with player

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,6 +4,7 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
     private bool isBallLaunched;
     private Rigidbody ballRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,12 +12,19 @@ public class BallController : MonoBehaviour
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall(){
         if(isBallLaunched) return;
 
         isBallLaunched = true;
+
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
